dd2f5af720d398b9dfaccb0320d98683ad45d7ff
Parsa Ghadimi <me@qti3e.com>
2018-09-22 18:44:38 +0330

    Implement tokenizer

--- src/token.c
+++ src/token.c
@@ -4,27 +4,56 @@
 #include <ctype.h>
 
 enum TokenType {
-  STRING_LITERAL,
-  IDENTIFIER,
-  OPEN_PARENTHESIS,
-  CLOSE_PARENTHESIS,
-  OPEN_BRACE,
-  CLOSE_BRACE,
-  OPEN_BRACKET,
-  CLOSE_BRACKET,
-  COMMENT,
   LINE_BREAK,
-  SEMICOLON,
+  // Many bytes
+  NUMERIC_LITERAL,
+  STRING_LITERAL,
+  COMMENT,
+  IDENTIFIER,
+  KEYWORD,
+  // Two byte
+  DOT_DOT, // ..
+  EQUALITY, // ==
+  INEQUALITY, // !=
+  LOGICAL_OR, // ||
+  LOGICAL_AND, // &&
+  BIT_OR_ASSIGN, // |=
+  BIT_XOR_ASSIGN, // ^=
+  BIT_NOT_ASSIGN, // ~=
+  AND_ASSIGN, // &=
+  PLUS_ASSIGN, // +=
+  MINUS_ASSIGN, // -=
+  TIMES_ASSIGN, // *=
+  DIV_ASSIGN, // /=
+  LEFT_SHIFT, // <<
+  RIGHT_SHIFT, // >>
+  GREATER_THAN_EQUAL, // >=
+  LESS_THAN_EQUAL, // <=
+  INCREMENT, // ++
+  DECREMENT, // --
+  // One byte
+  DOT, // .
+  OPEN_PARENTHESIS, // (
+  CLOSE_PARENTHESIS, // )
+  OPEN_BRACE, // {
+  CLOSE_BRACE, // }
+  OPEN_BRACKET, // [
+  CLOSE_BRACKET, // ]
+  SEMICOLON, // ;
   COLON, // :
   COMMA, // ,
-  DOT,
-  DOT_DOT,
-  // TODO(qti3e)
-  KEYWORD,
-  // TODO(qti3e) Maybe remove this and instead add an
-  // enum member for each OPERATOR?
-  OPERATOR,
-  NUMBER_LITERAL,
+  ASSIGNMENT, // =
+  BIT_OR, // |
+  BIT_AND, // &
+  BIT_XOR, // ^
+  BIT_NOT, // ~
+  NOT, // !
+  PLUS, // +
+  MINUS, // -
+  TIMES, // *
+  DIV, // /
+  GREATER_THAN, // >
+  LESS_THAN, // <
 };
 
 struct Token {
@@ -47,39 +76,6 @@ char* KEYWORDS[] = {
   // TODO(qti3e)
 };
 
-char* OPERATORS[] = {
-  "||",
-  "&&",
-  "==",
-  "|=",
-  "^=",
-  "!=",
-  "~=",
-  "&=",
-  "+=",
-  "-=",
-  "*=",
-  "/=",
-  "<=",
-  ">=",
-  "<<",
-  ">>",
-  "++",
-  "--",
-  "|",
-  "&",
-  "=",
-  "^",
-  "!",
-  "~",
-  "+",
-  "-",
-  "*",
-  "/",
-  "<",
-  ">",
-};
-
 char *tokenData(Token *token) {
   // Skip characters to the start.
   char *data = *token->code + token->start;
@@ -89,88 +85,45 @@ char *tokenData(Token *token) {
   return ptr;
 }
 
-void printToken(Token *token) {
-  int print_data = 1;
-  printf("{ pos: %d, type: ", token->start);
+char* tokenName(Token *token) {
   switch (token->type) {
-    case OPERATOR:
-      printf("OPERATOR");
-      break;
-    case NUMBER_LITERAL:
-      printf("NUMBER_LITERAL");
-      break;
-    case STRING_LITERAL:
-      printf("STRING_LITERAL");
-      break;
     case IDENTIFIER:
-      printf("IDENTIFIER");
-      break;
+      return "IDENTIFIER";
     case KEYWORD:
-      printf("KEYWORD");
-      break;
-    case OPEN_PARENTHESIS:
-      printf("OPEN_PARENTHESIS");
-      print_data = 0;
-      break;
-    case CLOSE_PARENTHESIS:
-      printf("CLOSE_PARENTHESIS");
-      print_data = 0;
-      break;
-    case OPEN_BRACE:
-      printf("OPEN_BRACE");
-      print_data = 0;
-      break;
-    case CLOSE_BRACE:
-      printf("CLOSE_BRACE");
-      print_data = 0;
-      break;
-    case OPEN_BRACKET:
-      printf("OPEN_BRACKET");
-      print_data = 0;
-      break;
-    case CLOSE_BRACKET:
-      printf("CLOSE_BRACKET");
-      print_data = 0;
-      break;
+      return "KEYWORD";
     case COMMENT:
-      printf("COMMENT");
-      break;
+      return "COMMENT";
+    case NUMERIC_LITERAL:
+    case STRING_LITERAL:
+      return "LITERAL";
+    case OPEN_PARENTHESIS:
+    case CLOSE_PARENTHESIS:
+      return "PARENTHESIS";
+    case OPEN_BRACE:
+    case CLOSE_BRACE:
+      return "BRACE";
+    case OPEN_BRACKET:
+    case CLOSE_BRACKET:
+      return "BRACKET";
     case LINE_BREAK:
-      printf("LINE_BREAK");
-      print_data = 0;
-      break;
-    case SEMICOLON:
-      printf("SEMICOLON");
-      print_data = 0;
-      break;
-    case COLON:
-      printf("COLON");
-      print_data = 0;
-      break;
-    case COMMA:
-      printf("COMMA");
-      print_data = 0;
-      break;
-    case DOT:
-      printf("DOT");
-      print_data = 0;
-      break;
-    case DOT_DOT:
-      printf("DOT_DOT");
-      print_data = 0;
-      break;
-  }
-  if (print_data) {
-    char *data = tokenData(token);
-    printf(", data: \"%s\" }\n", data);
-    free(data);
-  } else {
-    printf(" }\n");
+      return "LINE_BREAK";
+    default:
+      return "OPERATOR";
   }
 }
 
+void printToken(Token *token) {
+  printf(
+      ">> pos:\t%d,\ttype[0x%02lX]: %s,\tdata: <%s>\n",
+      token->start,
+      token->type,
+      tokenName(token),
+      tokenData(token)
+      );
+}
+
 int main() {
-  char *code = "x = 3;\n// test \nvar = 32 + x * 4; DA = 'Hello World'; c = 's\\' s'";
+  char *code = "if x |= 3;\n// test \nvar = 32 + x * 4; DA = 'Hello World'; c = 's\\' s'";
   printf("Code:\n%s\n", code);
 
   int num_tokens = 0;
@@ -187,6 +140,7 @@ int main() {
   int cursor = 0;
   // Current character under the cursor.
   char c;
+  char next_char;
 
   int skip_comments = 0;
 
@@ -205,7 +159,11 @@ int main() {
     }
 
     c = code[cursor];
+    next_char = code[cursor + 1];
     if (c == 0x00) break;
+    // If it's the last character, jump to check
+    // one byte tokens.
+    if (next_char == 0x00) goto one_byte;
 
     // Skip white spaces.
     if (isspace(c)) {
@@ -221,6 +179,19 @@ int main() {
       token.len = 1;
       insert_token = 1;
       continue;
+    }
+
+    // Handle NUMERIC_LITERAL
+    if (isdigit(c)) {
+      token.type = NUMERIC_LITERAL;
+      token.start = cursor;
+      token.len = 0;
+      insert_token = 1;
+      // Look for end of the number.
+      while (c != 0x00 && isdigit(c)) {
+        c = code[cursor + token.len + 1];
+        ++token.len;
+      }
     }
 
     // Handle STRING_LITERAL
@@ -268,116 +239,85 @@ int main() {
         c = code[cursor + token.len + 1];
         ++token.len;
       }
-      // Create the token.
-      // TODO(qti3e) Check if this is a keyword or not.
+      // Default to IDENTIFIER
       token.type = IDENTIFIER;
       token.start = cursor;
       insert_token = 1;
+      char* identifier = tokenData(&token);
+      size_t n = sizeof(KEYWORDS)/sizeof(KEYWORDS[0]);
+      for (int i = 0; i < n; ++i) {
+        if (strcmp(identifier, KEYWORDS[i]) == 0) {
+          token.type = KEYWORD;
+          break;
+        }
+      }
       continue;
     }
 
-    // Handle DOT_DOT
-    if (c == '.' && code[cursor + 1] == '.') {
-      token.type = DOT_DOT;
-      token.start = cursor;
-      token.len = 2;
-      insert_token = 1;
-      continue;
+    // Now look for tokens with a fixed size.
+    token.start = cursor;
+    insert_token = 1;
+
+two_byte:
+    // -- Two byte tokens
+    token.len = 2;
+
+    if (next_char == '=') {
+      switch (c) {
+        case '=': token.type = EQUALITY; continue;
+        case '!': token.type = INEQUALITY; continue;
+        case '|': token.type = BIT_OR_ASSIGN; continue;
+        case '^': token.type = BIT_XOR_ASSIGN; continue;
+        case '~': token.type = BIT_NOT_ASSIGN; continue;
+        case '&': token.type = AND_ASSIGN; continue;
+        case '+': token.type = PLUS_ASSIGN; continue;
+        case '-': token.type = MINUS_ASSIGN; continue;
+        case '*': token.type = TIMES_ASSIGN; continue;
+        case '/': token.type = DIV_ASSIGN; continue;
+        case '>': token.type = GREATER_THAN_EQUAL; continue;
+        case '<': token.type = LESS_THAN_EQUAL; continue;
+      }
     }
 
-    // Handle DOT
-    if (c == '.') {
-      token.type = DOT;
-      token.start = cursor;
-      token.len = 1;
-      insert_token = 1;
-      continue;
-    }
+    if (c == '.' && next_char == '.') { token.type = DOT_DOT; continue; }
+    if (c == '|' && next_char == '|') { token.type = LOGICAL_OR; continue; }
+    if (c == '&' && next_char == '&') { token.type = LOGICAL_AND; continue; }
+    if (c == '<' && next_char == '<') { token.type = LEFT_SHIFT; continue; }
+    if (c == '>' && next_char == '>') { token.type = RIGHT_SHIFT; continue; }
+    if (c == '+' && next_char == '+') { token.type = INCREMENT; continue; }
+    if (c == '-' && next_char == '-') { token.type = DECREMENT; continue; }
 
-    // Handle OPEN_PARENTHESIS
-    if (c == '(') {
-      token.type = OPEN_PARENTHESIS;
-      token.start = cursor;
-      token.len = 1;
-      insert_token = 1;
-      continue;
-    }
+one_byte:
+    // -- One byte tokens
+    token.len = 1;
 
-    // Handle CLOSE_PARENTHESIS
-    if (c == ')') {
-      token.type = CLOSE_PARENTHESIS;
-      token.start = cursor;
-      token.len = 1;
-      insert_token = 1;
-      continue;
-    }
-
-    // Handle OPEN_BRACKET
-    if (c == '[') {
-      token.type = OPEN_BRACKET;
-      token.start = cursor;
-      token.len = 1;
-      insert_token = 1;
-      continue;
-    }
-
-    // Handle CLOSE_BRACKET
-    if (c == ']') {
-      token.type = CLOSE_BRACKET;
-      token.start = cursor;
-      token.len = 1;
-      insert_token = 1;
-      continue;
-    }
-
-    // Handle OPEN_BRACE
-    if (c == '{') {
-      token.type = OPEN_BRACE;
-      token.start = cursor;
-      token.len = 1;
-      insert_token = 1;
-      continue;
-    }
-
-    // Handle CLOSE_BRACE
-    if (c == '}') {
-      token.type = CLOSE_BRACE;
-      token.start = cursor;
-      token.len = 1;
-      insert_token = 1;
-      continue;
-    }
-
-    // Handle SEMICOLON
-    if (c == ';') {
-      token.type = SEMICOLON;
-      token.start = cursor;
-      token.len = 1;
-      insert_token = 1;
-      continue;
-    }
-
-    // Handle COLON
-    if (c == ':') {
-      token.type = SEMICOLON;
-      token.start = cursor;
-      token.len = 1;
-      insert_token = 1;
-      continue;
-    }
-
-    // Handle COMMA
-    if (c == ',') {
-      token.type = COMMA;
-      token.start = cursor;
-      token.len = 1;
-      insert_token = 1;
-      continue;
-    }
+    if (c == '.') { token.type = DOT; continue; }
+    if (c == '(') { token.type = OPEN_PARENTHESIS; continue; }
+    if (c == ')') { token.type = CLOSE_PARENTHESIS; continue; }
+    if (c == '{') { token.type = OPEN_BRACE; continue; }
+    if (c == '}') { token.type = CLOSE_BRACE; continue; }
+    if (c == '[') { token.type = OPEN_BRACKET; continue; }
+    if (c == ']') { token.type = CLOSE_BRACKET; continue; }
+    if (c == ';') { token.type = SEMICOLON; continue; }
+    if (c == ':') { token.type = SEMICOLON; continue; }
+    if (c == ',') { token.type = COMMA; continue; }
+    if (c == '=') { token.type = ASSIGNMENT; continue; }
+    if (c == '|') { token.type = BIT_OR; continue; }
+    if (c == '&') { token.type = BIT_AND; continue; }
+    if (c == '^') { token.type = BIT_XOR; continue; }
+    if (c == '~') { token.type = BIT_NOT; continue; }
+    if (c == '!') { token.type = NOT; continue; }
+    if (c == '+') { token.type = PLUS; continue; }
+    if (c == '-') { token.type = MINUS; continue; }
+    if (c == '*') { token.type = TIMES; continue; }
+    if (c == '/') { token.type = DIV; continue; }
+    if (c == '>') { token.type = GREATER_THAN; continue; }
+    if (c == '<') { token.type = LESS_THAN; continue; }
 
     // TODO(qti3e) Error <UNEXPECTED_CHARACTER>
     // But for now let's just skip this unknown character:
     cursor++;
+    insert_token = 0;
   } while (1);
 
   printf("\nTokens:\n");
