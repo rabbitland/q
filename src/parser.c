fe0318083096b3a2c1c4e0578dc652378c5cfb63
Parsa Ghadimi <me@qti3e.com>
2018-09-28 22:45:53 +0330

    Add ImportStatemnet

--- src/parser.c
+++ src/parser.c
@@ -1,45 +1,80 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <stddef.h>
+#include <string.h>
 #include "token.h"
+#include "parser.h"
 
-enum NodeKind {
-  ImportStatemnet,
-  VariableDeclaration,
-};
+void print_node(Node *node) {
+  printf(">> Node\tKIND: %s\n", KIND_STRINGS[node->kind]);
+}
 
-struct ImportStatemnet {
-  char *module_name;
-};
+NodeArray* parse_tokens(TokenArray *tokens, char **code) {
+  return source_file(tokens, code, 0);
+} 
 
-struct VariableDeclaration {
-  int x;
-};
+NodeArray* source_file(TokenArray *tokens, char **code, size_t cursor) {
+  Node *tmp_node;
+  Node *node = NULL;
+  size_t node_length = 0;
 
-struct Node {
-  enum NodeKind kind;
-  union Data {
-    struct ImportStatemnet import_statemnet;
-    struct VariableDeclaration variable_declaration;
-  } data;
-} typedef Node;
+  size_t node_array_size = offsetof(NodeArray, nodes);
+  void *childs = malloc(node_array_size);
+  size_t count = 0; // Number of nodes in the array.
+  size_t length = 0; // number of tokens in the array. == tokens->count
 
-void parse_tokens(TokenArray *tokens, char **code) {
-  printf("Number of tokens: %zu\n", tokens->count);
-  for (int i = 0; i < tokens->count; ++i) {
-    printToken(&tokens->tokens[i], code);
-  }
+  do {
+    if (node) {
+      // Insert node to the array.
+      count += 1;
+      length += node_length;
+      childs = realloc(childs, node_array_size + count * sizeof(Node));
+      memcpy(childs + node_array_size + (count - 1) * sizeof(Node), &node, sizeof(Node));
+      cursor += node_length;
+      // Debug...
+      print_node(node);
+      // Reset data
+      node = NULL;
+      node_length = 0;
+    }
 
-  void *nodes = malloc(0);
-  int cursor = 0;
+    if (tokens->tokens[cursor].type == LINE_BREAK) {
+      cursor += 1;
+      continue;
+    }
 
-  while (cursor < tokens->count) {
-    if (tokens->tokens[cursor].type == USE_KEYWORD) {
-      if (tokens->tokens[cursor + 1].type == COLON_COLON) {
-        printf("Import!!!\n");
-        exit(1);
-      }
-      printToken(&tokens->tokens[cursor], code);
+    printToken(&tokens->tokens[cursor], code);
+
+    tmp_node = import_statemnet(tokens, code, cursor);
+    if (tmp_node && tmp_node->length > node_length) {
+      node = tmp_node;
+      node_length = node->length;
+    }
+
+    if (!node) {
+      printf("Unecpected token `%s`.\n",
+          tokenName(&tokens->tokens[cursor]));
       exit(-1);
     }
-  }
+  } while(1);
+
+  NodeArray *ret = childs;
+  ret->count = count;
+  ret->length = length;
+  return childs;
+}
+
+Node *import_statemnet(TokenArray *tokens, char **code, size_t cursor) {
+  if (tokens->tokens[cursor].type != USE_KEYWORD) return NULL;
+  if (tokens->tokens[cursor + 1].type != COLON_COLON) return NULL;
+  if (tokens->tokens[cursor + 2].type != IDENTIFIER) return NULL;
+  if (tokens->tokens[cursor + 3].type != SEMICOLON) return NULL;
+  char *module_name = tokenData(&tokens->tokens[cursor + 2], code);
+  Node *node = malloc(sizeof(Node));
+  node->kind = ImportStatemnetKind;
+  node->length = 4;
+  node->data.import_statemnet = (struct ImportStatemnet){
+    module_name
+  };
+  return node;
 }
--- src/parser.h
+++ src/parser.h
@@ -1,6 +1,47 @@
 #ifndef __Q_PARSER__
 #define __Q_PARSER__
 
-void parse_tokens(TokenArray *tokens, char **code);
+#define GENERATE_ENUM(ENUM) ENUM,
+#define GENERATE_STRING(STRING) #STRING,
+
+#define FOREACH_KIND(KIND) \
+  KIND(ImportStatemnetKind) \
+
+enum NodeKind {
+  FOREACH_KIND(GENERATE_ENUM)
+};
+
+static char *KIND_STRINGS[] = {
+  FOREACH_KIND(GENERATE_STRING)
+};
+
+struct ImportStatemnet {
+  char *module_name;
+};
+
+struct VariableDeclaration {
+  char *name;
+  // TODO(qti3e) Expression.
+};
+
+struct Node {
+  enum NodeKind kind;
+  size_t length; // Number of tokens for this Node.
+  union {
+    struct ImportStatemnet import_statemnet;
+    struct VariableDeclaration variable_declaration;
+  } data;
+} typedef Node;
+
+struct NodeArray {
+  size_t count;
+  size_t length; // sumof(this.nodes[x].length)
+  Node nodes[];
+} typedef NodeArray;
+
+NodeArray* parse_tokens(TokenArray *tokens, char **code);
+
+NodeArray* source_file(TokenArray *tokens, char **code, size_t cursor);
+Node *import_statemnet(TokenArray *tokens, char **code, size_t cursor);
 
 #endif
